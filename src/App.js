e234fe80189d84551d664ef14441fa134a1167a6
Talha_Aydin <mt.aydin@outlook.com>
2021-10-18 20:21:47 +0300

    router set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.scss';
 import Login from './components/Login/Login';
 import SignUp from './components/Signup/SignUp';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <SignUp></SignUp>
-      <Login></Login>
+      {/* <Header /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignUp></SignUp>
+          </Route>
+          <Route exact path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
